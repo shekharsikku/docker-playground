40df82d2cfef9ef2920117438eeb4d5a08533aa3
Shekhar Sharma <shekharsharma.ss9151@gmail.com>
2025-05-05 09:33:03 +0530

    feat(express) utils for return response

---
+++ src/utils/response.ts
@@ -0,0 +1,48 @@
+import { Response } from "express";
+
+class HttpError extends Error {
+  public code: number;
+  public message: string;
+
+  constructor(code: number, message: string, stack: string = "") {
+    super(message);
+
+    this.code = code;
+    this.message = message;
+    this.stack = stack;
+  }
+}
+
+type TypeResponse<T = any, E = any> = {
+  success: boolean;
+  message: string;
+  code?: number;
+  data?: T;
+  error?: E;
+};
+
+const ApiError = (
+  res: Response,
+  code: number,
+  message: string,
+  error: any = null
+) => {
+  const response: TypeResponse<null, any> = { success: false, message };
+
+  if (error) response.error = error;
+  res.status(code).json(response);
+};
+
+const ApiResponse = (
+  res: Response,
+  code: number,
+  message: string,
+  data: any = null
+) => {
+  const response: TypeResponse<any, null> = { success: true, message };
+
+  if (data) response.data = data;
+  res.status(code).json(response);
+};
+
+export { HttpError, ApiError, ApiResponse };
