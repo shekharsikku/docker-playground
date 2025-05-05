149e96bd58c9f58ff55daaf1cf5000bc58e72ffa
Shekhar Sharma <shekharsharma.ss9151@gmail.com>
2025-05-05 09:36:36 +0530

    feat(middleware) for check data exists or not

---
+++ src/middlewares/restaurant.ts
@@ -0,0 +1,22 @@
+import type { Request, Response, NextFunction } from "express";
+import { initializeRedisClient } from "../utils/client.js";
+import { restaurantKeyById } from "../utils/keys.js";
+import { ApiError } from "../utils/response.js";
+
+export const checkRestaurantExists = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { restaurantId } = req.params;
+  if (!restaurantId) {
+    return ApiError(res, 400, "Restaurant Id not found!");
+  }
+  const client = await initializeRedisClient();
+  const restaurantKey = restaurantKeyById(restaurantId);
+  const exists = await client.exists(restaurantKey);
+  if (!exists) {
+    return ApiError(res, 404, "Restaurant not found!");
+  }
+  next();
+};
